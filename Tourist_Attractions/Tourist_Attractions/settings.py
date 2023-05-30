2d80b547539341b91f31b588d89b5eda2060c4ea
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-30 09:55:40 +0200

    Tourist_Attractions_step_01

--- Tourist_Attractions/Tourist_Attractions/settings.py
+++ Tourist_Attractions/Tourist_Attractions/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'touristAttractions.apps.touristAttractionsConfig'
 ]
 
 MIDDLEWARE = [
